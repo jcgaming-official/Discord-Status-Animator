8e24bf6c98fa29d4c5cd684ac39de56a2d54c29b
jcgaming-official <77482435+jcgaming-official@users.noreply.github.com>
2021-11-30 01:53:59 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://bit.lt/JcGaming"/>'
+    return '<meta http-equiv="refresh" content="0; URL=https://bit.ly/JcGaming"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
